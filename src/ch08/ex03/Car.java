4ed072bab2e485f14b92217d47029c3a78d53a22
Heeseok Lee <2dilution2@gmail.com>
2023-05-20 17:09:13 +0900

    2023.05.20 주말 보충

--- src/ch08/ex03/Car.java
+++ src/ch08/ex03/Car.java
@@ -2,13 +2,64 @@ package ch08.ex03;
 
 public class Car {
 	
-	private String company;
-	private String carName;
-	private String carColor;
-	private int carDoor;
-	private double carWeight;
+	private String company; 
+	private String carName; 
+	private int carMaxSpeed; 
+	private double carWeight; 
+	
+	
+	// getter, setter를 생성해서 각 필드의 값을 넣고(setter) 출력 (getter) 
+	public String getCompany() {
+		return company;
+	}
+
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+
+	public String getCarName() {
+		return carName;
+	}
+
+
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+
+
+	public int getCarMaxSpeed() {
+		return carMaxSpeed;
+	}
+
+
+	public void setCarMaxSpeed(int carMaxSpeed) {
+		this.carMaxSpeed = carMaxSpeed;
+	}
+
+
+	public double getCarWeight() {
+		return carWeight;
+	}
+
+
+	public void setCarWeight(double carWeight) {
+		this.carWeight = carWeight;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	public static void main(String[] args) {
+		// Car객체를 생성후 각 메소드에 setter를 호출해서 값을 넣고, 
+			// getter를 사용해서 출력 
+		
+		
 		
 	}
 }
---
+++ src/ch08/ex04/Member.java
@@ -0,0 +1,8 @@
+package ch08.ex04;
+
+public class Member {
+
+	
+	
+
+}
